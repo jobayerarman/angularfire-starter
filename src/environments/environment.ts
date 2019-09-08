da017dc5c4b8f0bea83a15169b9faca350d8c1ba
Jobayer Arman <jobayer.arman@gmail.com>
2019-09-09 04:01:31 +0600

    :wrench: config(firebase): Add Firebase config to environments variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCuc9JpWSEXsvxUMBSbifEiD0BCdgOtkDM',
+    authDomain: 'angularfire-starter-6cf20.firebaseapp.com',
+    databaseURL: 'https://angularfire-starter-6cf20.firebaseio.com',
+    projectId: 'angularfire-starter-6cf20',
+    storageBucket: '',
+    messagingSenderId: '595917780465'
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
