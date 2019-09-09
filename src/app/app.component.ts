c117f5ac6cbf556b28ded2add9da7f3571a9b056
Jobayer Arman <jobayer.arman@gmail.com>
2019-09-09 16:18:05 +0600

    update app-routing module
    now everything works just fine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-firebase-crud';
+export class AppComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
 }
