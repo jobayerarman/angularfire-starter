553e1deff422fcca05e311f00a7d516a5a91eb8f
Jobayer Arman <jobayer.arman@gmail.com>
2019-09-09 09:20:27 +0600

    add new user ngComponent

---
+++ src/app/user/new-user.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { MatDialog } from '@angular/material';
+import { AvatarDialogComponent } from "../user/avatar-dialog.component";
+import { Router } from '@angular/router';
+import { FirebaseService } from '../services/firebase.service';
+
+@Component({
+  selector: 'app-new-user',
+  templateUrl: './new-user.component.html',
+  styleUrls: ['./new-user.component.scss']
+})
+export class NewUserComponent implements OnInit {
+
+  exampleForm: FormGroup;
+  avatarLink: string = "https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg";
+
+  validation_messages = {
+    'name': [
+      { type: 'required', message: 'Name is required.' }
+    ],
+    'surname': [
+      { type: 'required', message: 'Surname is required.' }
+    ],
+    'age': [
+      { type: 'required', message: 'Age is required.' },
+    ]
+  };
+
+  constructor(
+    private fb: FormBuilder,
+    public dialog: MatDialog,
+    private router: Router,
+    public firebaseService: FirebaseService
+  ) { }
+
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.exampleForm = this.fb.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      age: ['', Validators.required]
+    });
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(AvatarDialogComponent, {
+      height: '400px',
+      width: '400px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.avatarLink = result.link;
+      }
+    });
+  }
+
+  resetFields() {
+    this.avatarLink = "https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg";
+    this.exampleForm = this.fb.group({
+      name: new FormControl('', Validators.required),
+      surname: new FormControl('', Validators.required),
+      age: new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmit(value) {
+    this.firebaseService.createUser(value, this.avatarLink)
+      .then(
+        res => {
+          this.resetFields();
+          this.router.navigate(['/home']);
+        }
+      )
+  }
+
+}
