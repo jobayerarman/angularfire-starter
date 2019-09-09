7685b0d3a106944bbba43f7d4d3abe00e57da686
Jobayer Arman <jobayer.arman@gmail.com>
2019-09-09 09:18:44 +0600

    add new ngComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 import { RouterModule } from '@angular/router';
 import { rootRouterConfig } from './app.routes';
-import { ReactiveFormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
+import { AvatarDialogComponent } from './user/avatar-dialog.component';
+import { EditUserComponent } from './user/edit-user.component';
+import { EditUserResolver } from './user/edit-user.resolver';
+import { NewUserComponent } from './user/new-user.component';
+import { HomeComponent } from './home/home.component';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AngularFireStorageModule } from '@angular/fire/storage';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { FirebaseService } from './services/firebase.service';
+
 import { environment } from '../environments/environment';
 
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { UserComponent } from './user/user.component';
-
-import { UserResolver } from './user/user.resolver';
-import { AuthGuard } from './core/auth.guard';
-import { AuthService } from './core/auth.service';
-import { UserService } from './core/user.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatInputModule, MatSliderModule, MatDialogModule } from '@angular/material';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    UserComponent,
-    RegisterComponent
+    AvatarDialogComponent,
+    EditUserComponent,
+    NewUserComponent,
+    HomeComponent
   ],
+  entryComponents: [AvatarDialogComponent],
   imports: [
     BrowserModule,
+    FormsModule,
     ReactiveFormsModule,
     RouterModule.forRoot(rootRouterConfig, { useHash: false }),
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
-    AngularFireAuthModule, // imports firebase/auth, only needed for auth features
+    AngularFirestoreModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSliderModule,
+    MatDialogModule
   ],
-  providers: [AuthService, UserService, UserResolver, AuthGuard],
-  bootstrap: [AppComponent]
+  providers: [FirebaseService, EditUserResolver],
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,12 @@
 import { Routes } from '@angular/router';
-
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { UserComponent } from './user/user.component';
-import { UserResolver } from './user/user.resolver';
-import { AuthGuard } from './core/auth.guard';
+import { HomeComponent } from './home/home.component';
+import { NewUserComponent } from './user/new-user.component';
+import { EditUserComponent } from './user/edit-user.component';
+import { EditUserResolver } from './user/edit-user.resolver';
 
 export const rootRouterConfig: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
-  { path: 'register', component: RegisterComponent, canActivate: [AuthGuard] },
-  { path: 'user', component: UserComponent, resolve: { data: UserResolver } }
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'new-user', component: NewUserComponent },
+  { path: 'details/:id', component: EditUserComponent, resolve: { data: EditUserResolver } }
 ];
