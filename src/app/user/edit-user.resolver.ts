0d5c5e479c48cff254a4d844550e335895e1f0e4
Jobayer Arman <jobayer.arman@gmail.com>
2019-09-09 09:23:40 +0600

    add user edit resolver ngModule

---
+++ src/app/user/edit-user.resolver.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, ActivatedRoute } from "@angular/router";
+import { FirebaseService } from '../services/firebase.service';
+
+@Injectable()
+export class EditUserResolver implements Resolve<any> {
+
+  constructor(public firebaseService: FirebaseService) { }
+
+  resolve(route: ActivatedRouteSnapshot, ) {
+
+    return new Promise((resolve, reject) => {
+      let userId = route.paramMap.get('id');
+      this.firebaseService.getUser(userId)
+        .subscribe(
+          data => {
+            resolve(data);
+          }
+        );
+    })
+  }
+}
